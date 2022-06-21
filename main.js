e747bd7ecf2774005bba49f6dc7f383461d3f2eb
faus34 <faustinogm1997@gmail.com>
2022-06-21 16:39:50 -0500

    basic layout

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=20ace127-d962-4106-b494-eb7122da9b7c';
 
 // fetch(URL)
 //     .then(response => response.json())
@@ -10,10 +10,13 @@ const URL = 'https://api.thecatapi.com/v1/images/search';
 async function cats (){
     const response = await fetch(URL);
     const data = await response.json();
-    const img = document.getElementById('random-cat');
-    img.src = data[0].url;
+    const img1 = document.getElementById('random-cat1');
+    const img2 = document.getElementById('random-cat2');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
 }
 
-const button = document.getElementById('cat-button');
+cats();
 
+const button = document.getElementById('cat-button');
 button.onclick = cats;
