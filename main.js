fd135e323a5cbf71e74fbecfe52942d003aff420
faus34 <faustinogm1997@gmail.com>
2022-06-22 17:33:08 -0500

    save favorite michis feature added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const URL = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=20ace127-d962-4106-b494-eb7122da9b7c';
-const URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites/search?limit=2&api_key=20ace127-d962-4106-b494-eb7122da9b7c';
+const URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=20ace127-d962-4106-b494-eb7122da9b7c';
 const spanError = document.getElementById('error');
 // fetch(URL)
 //     .then(response => response.json())
@@ -16,8 +16,17 @@ async function loadRandomMichis (){
     }else {
         const img1 = document.getElementById('random-cat1');
         const img2 = document.getElementById('random-cat2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
+
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => {
+            console.log('btn 1 click',data)
+            saveFavoriteMichis(data[0].id)}
+        btn2.onclick = () => {    
+            saveFavoriteMichis(data[1].id)} 
     }
 }
 
@@ -28,30 +37,60 @@ async function loadFavoriteMichis (){
     if(response.status !== 200){
         spanError.innerHTML = "Hubo un error" + response.status +data.message;
     }else {
-        const img1 = document.getElementById('fav-cat1');
-        const img2 = document.getElementById('fav-cat2');
-        img1.src = data[0].url;
-        img2.src = data[1].url;
+        data.forEach(michi => {
+            const section =  document.getElementById('favorites');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnTxt = document.createTextNode('Remove from favorites');
+
+            img.src = michi.image.url;
+            img.classList.add('fav-cat-img');
+            article.classList.add('cat-container');
+            btn.appendChild(btnTxt);
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        });
     }
 }
 
-async function saveFavoriteMichis (){
-    const res = await fetch(URL_FAVORITES, {
+async function saveFavoriteMichis (id){
+    const response = await fetch(URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type':'application/json'
         },
         body: JSON.stringify({
-            image_id: "dje"
+            image_id: id
         })
     });
     const data = await response.json();
+    console.log(data);
+    if(response.status !== 200){
+        spanError.innerHTML = "Hubo un error" + response.status +data.message;
+    }
+}
+
+async function deleteFavoriteMichis (id){
+    const response = await fetch(URL_FAVORITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            image_id: id
+        })
+    });
+    const data = await response.json();
+    console.log(data);
     if(response.status !== 200){
         spanError.innerHTML = "Hubo un error" + response.status +data.message;
     }
 }
 
 loadRandomMichis();
+loadFavoriteMichis();
 
 const button = document.getElementById('cat-button');
 button.onclick = loadRandomMichis;
